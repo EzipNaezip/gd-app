b425cf388629a97a89c254cfc311fede553c2444
Dongmin <pdm001125@naver.com>
2023-04-05 19:51:54 +0900

    FEAT : Main Screen 구현
    검색 전, 후 화면 구현
    검색창 : 검색기능, 전체삭제기능
    검색 전 화면 : 임시로고 사용
    검색 후 화면 : 로딩창 구현. Api와 연동 안됨. Api 연동하면 로딩창에서 결과창으로 이동하는 코드 작성 예정. 결과 사진 클릭시 확대기능.북마크 버튼.

--- app/src/main/java/com/example/gd/Screens/MainScreen.kt
+++ app/src/main/java/com/example/gd/Screens/MainScreen.kt
@@ -1,38 +1,54 @@
-//@file:OptIn(ExperimentalComposeUiApi::class)
-
 package com.example.gd.Screens
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInVertically
-import androidx.compose.animation.slideOutVertically
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.*
 import androidx.compose.material.TextField
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavHostController
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.gd.Effects.LoadingShimmerEffect
 import com.example.gd.R
+import com.example.gd.ui.IconPack
+import com.example.gd.ui.iconpack.Home
+import com.example.gd.ui.theme.suite
+import kotlinx.coroutines.*
 
 @Composable
 fun MainScreen(navController: NavHostController) {
-    var showLogo by remember { mutableStateOf(true) }
+    var searchState by remember { mutableStateOf(true) }
+
+    // 테스트용 이미지. 추후 변경 예정. listOf()로 만들고, Api 호출전에 다시 초기화 하고, 값을 하나씩 넣는 방법 사용
+    val testImage: List<Int> =
+        listOf(R.drawable.logo, R.drawable.logo, R.drawable.logo, R.drawable.logo)
 
     Box(
         modifier = Modifier
@@ -40,60 +56,50 @@ fun MainScreen(navController: NavHostController) {
             .background(MaterialTheme.colors.primary)
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .border(0.dp, Color.Transparent),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-            AnimatedVisibility(
-                visible = showLogo,
-                enter = slideInVertically(initialOffsetY = { -it }),
-                exit = slideOutVertically(targetOffsetY = { -it })
+            Spacer(modifier = Modifier.padding(8.dp))
+            Card(
+                modifier = Modifier.border(0.dp, Color.Transparent),
+                elevation = 0.dp
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "logo",
-                    modifier = Modifier.padding(vertical = 50.dp)
-                )
-            }
+                if (searchState) {
+                    Image( //로고
+                        painter = painterResource(id = R.drawable.logo),
+                        contentDescription = "logo",
+                        modifier = Modifier.padding(vertical = 200.dp),
+                        alpha = 0.2f
+                    )
+                }
 
-            AnimatedVisibility(
-                visible = true,
-                enter = slideInVertically(initialOffsetY = { -it }),
-                exit = slideOutVertically(targetOffsetY = { -it })
-            ) {
-                SearchBar(
+                SearchBar( // 검색창
                     onSearch = {
-                        showLogo = false
-                        // 검색어로 검색한 결과 화면으로 이동하는 코드를 작성합니다.
+                        searchState = false
+                        // 검색어로 검색한 결과 나타내는 코드 예정 => Api 호출
                     },
                     modifier = Modifier.fillMaxWidth()
                 )
             }
 
-//            Box(
-//                modifier = Modifier
-//                    .padding(horizontal = 16.dp)
-//                    .offset(y = if (showLogo) 0.dp else (-10).dp)
-//                    .animateContentSize(animationSpec = tween(400))
-//            ) {
-//                SearchBar(
-//                    onSearch = {
-//                        showLogo = false
-//                        // 검색어로 검색한 결과 화면으로 이동하는 코드를 작성합니다.
-//                    },
-//                    modifier = Modifier.fillMaxWidth()
-//                )
-//            }
-//            SearchBar(
-//                onSearch = {
-//                    showLogo = false
-//                    // 검색어로 검색한 결과 화면으로 이동하는 코드를 작성합니다.
-//                },
-//                modifier = Modifier
-//                    .padding(horizontal = 16.dp)
-//                    .offset(y = if (showLogo) 0.dp else (-80).dp)
-//                    .animateContentSize(animationSpec = tween(400))
-//            )
+            // api를 통해서 아래에 넣을 값을 가져온 후 아래 코드 실행
+            // api를 통해서 가져오는 동안 로딩창 실행
+
+            // searchState가 false가 되면 로딩창 실행.
+            // 로딩창 하는동안 api를 통해 값 가져옴
+            // 값을 받으면 실행
+
+            if (!searchState) {
+                LoadingShimmerEffect()
+
+                if (false){ // API사용해서 값 가져오면 실행.
+                    SearchResult(testImage)
+                }
+            }
+
         }
     }
 }
@@ -101,38 +107,28 @@ fun MainScreen(navController: NavHostController) {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchBar(onSearch: (String) -> Unit, modifier: Modifier) {
-    val context = LocalContext.current
     var searchText by remember { mutableStateOf("") }
-
+    val focusManager = LocalFocusManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
 
     TextField(
         value = searchText,
         onValueChange = { searchText = it },
-//        onKeyEvent = { keyEvent ->
-//            if (keyEvent.key == Key.Enter) {
-//                onSearch(searchText) // 검색어로 검색한 결과 화면으로 이동
-//                keyboardController?.hide() // 키보드 숨기기
-//                true
-//            } else {
-//                false
-//            }
-//        },
         placeholder = { Text("검색어를 입력하세요") },
         modifier = Modifier
             .fillMaxWidth()
             .height(90.dp)
             .padding(16.dp)
-            .background(Color.White)
-            .onKeyEvent { keyEvent ->
-                if (keyEvent.key == Key.Enter) {
-                    onSearch(searchText) // 검색어로 검색한 결과 화면으로 이동
-                    keyboardController?.hide() // 키보드 숨기기
-                    true
-                } else {
-                    false
+            .background(Color.White),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                if (searchText.isNotEmpty()) {
+                    onSearch(searchText)
                 }
-            },
+                focusManager.clearFocus()
+                keyboardController?.hide()
+            }),
         shape = RoundedCornerShape(16.dp),
         colors = TextFieldDefaults.textFieldColors(
             textColor = Color.Gray,
@@ -142,7 +138,6 @@ fun SearchBar(onSearch: (String) -> Unit, modifier: Modifier) {
             unfocusedIndicatorColor = Color.Transparent,
             disabledIndicatorColor = Color.Transparent
         ),
-        singleLine = true,
         leadingIcon = {
             Box(
                 modifier = Modifier.size(24.dp),
@@ -154,6 +149,98 @@ fun SearchBar(onSearch: (String) -> Unit, modifier: Modifier) {
                     tint = Color.Gray
                 )
             }
+        },
+        trailingIcon = {
+            Icon(Icons.Default.Clear,
+                contentDescription = "clear text",
+                modifier = Modifier
+                    .clickable {
+                        searchText = ""
+                    }
+            )
         }
     )
+}
+
+@Composable
+fun ImageScreen(@DrawableRes imageId: Int) {
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = "Image",
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { isExpanded = true }
+                .clip(RoundedCornerShape(8.dp))
+        )
+        if (isExpanded) {
+            Dialog(onDismissRequest = { isExpanded = false }) {
+                Image(
+                    painter = painterResource(id = imageId),
+                    contentDescription = "Expanded Image",
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun SearchResult(testImage: List<Int>){
+    Column() {
+        LazyVerticalGrid(
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 15.dp),
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(testImage) { photo ->
+                ImageScreen(photo)
+            }
+        }
+
+        // 검색결과가 너무 길어질 경우 Surface로 구분할수도
+        Text(
+            text = "이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 \n\n" +
+                    "이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 \n" +
+                    "이런 내용 저런 내용 요런 내용 조런 내용 이런 내용 저런 내용 요런 내용 조런 내용 ",
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 15.dp),
+            color = Color.Black,
+            fontFamily = suite,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 15.sp
+        )
+
+        Text(
+            text = "#Tag1 #Tag2 #Tag3",
+            modifier = Modifier.padding(vertical = 15.dp, horizontal = 15.dp),
+            color = Color.Black,
+            fontFamily = suite,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 15.sp
+        )
+
+        // 북마크 버튼 사용. 저장되지 않으면 비어있는 북마크, 저장된 정보면 채워진 북마크 모양.
+        // 누를때마다 state 변경. 빔 -> 참 /  참 -> 빔
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Button(
+                elevation = ButtonDefaults.elevation(0.dp),
+                shape = CircleShape,
+                onClick = { /*TODO*/ }, // 스낵바 구현
+            ) {
+                Icon(
+                    imageVector = IconPack.Home,
+                    contentDescription = "Bookmark",
+                    modifier = Modifier
+                        .width(26.dp)
+                        .height(26.dp)
+                )
+            }
+        }
+    }
 }

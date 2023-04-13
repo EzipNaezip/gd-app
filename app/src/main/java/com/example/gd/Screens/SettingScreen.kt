e55088ac7fb96cb4a0d291757854382cef773af1
Jinseo Jeong <104565869+JinNitt@users.noreply.github.com>
2023-04-13 20:09:17 +0900

    FEAT : Profile Edit Screen 구현
    공통 테마 적용
    이름, 아이디, 소개 등 텍스트란 추가
    텍스트 내부 패딩 제거

--- app/src/main/java/com/example/gd/Screens/SettingScreen.kt
+++ app/src/main/java/com/example/gd/Screens/SettingScreen.kt
@@ -5,35 +5,39 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.DarkGray
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.input.key.Key
-import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.gd.R
-import com.example.gd.ui.theme.Black
 import com.example.gd.ui.theme.suite
 import kotlinx.coroutines.launch
+
 
 var settingScreen by mutableStateOf("default")
 
@@ -45,26 +49,11 @@ fun SettingScreen( //설정 화면
     names: List<String> = listOf("프로필 수정", "회원 탈퇴", "로그아웃", "정보")
 ) {
     Column(modifier = Modifier.fillMaxHeight()) {
-        MaterialTheme { //맨 위의 앱 바
-            TopAppBar {
-                Row(
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        text = "설정",
-                        fontFamily = suite,
-                        fontWeight = FontWeight.ExtraBold,
-                        fontSize = 20.sp,
-                        color = Color.Black
-                    )
-                }
-            }
-        }
+        //TopAppBarFormat("설정")
 
         for (name in names) { //설정 목록 버튼 4개 생성
             Surface(
-                modifier = Modifier.background(MaterialTheme.colors.primary)
+                modifier = Modifier.background(colors.primary)
             ) {
                 Column(modifier = Modifier.fillMaxWidth()) {
                     Button(
@@ -116,13 +105,136 @@ fun SettingScreen( //설정 화면
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+@Preview
+fun TopAppBarFormat() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                contentColor = colors.onPrimary,
+                backgroundColor = colors.primary,
+                title = {
+                    Text(
+                        text = "titleText",
+                        fontFamily = suite,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back Button",
+                            tint = colors.onPrimary
+                        )
+                    }
+                },
+                elevation = 8.dp,
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Check,
+                            contentDescription = "Save Button",
+                            tint = colors.primaryVariant
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column( //페이지 내용
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp)
+                .padding(horizontal = 16.dp)
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(CircleShape)
+                        .border(1.dp, Color.LightGray, CircleShape)
+                )
+            }
+            Button(
+                onClick = { /* 사진 변경 기능 실행 */ },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(vertical = 4.dp),
+                elevation = ButtonDefaults.elevation(
+                    defaultElevation = 0.dp,
+                    pressedElevation = 0.dp
+                ),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Transparent)
+            ) {
+                Text(
+                    text = "프로필 사진 변경",
+                    fontFamily = suite,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 15.sp,
+                    color = Color.Blue
+                )
+            }
+            Column {
+                TextFieldFormat("이름")
+                TextFieldFormat("사용자 아이디") //인스타의 @sample_test
+                TextFieldFormat("소개")
+                TextFieldFormat("링크")
+                // + 나이, 성별
+            }
+        }
+    }
+}
+
+@Composable
+fun TextFieldFormat(fieldTitle: String) {
+    var userValue by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(
+            text = fieldTitle,
+            fontFamily = suite,
+            fontWeight = FontWeight.Normal,
+            fontSize = 15.sp,
+            color = Color.Gray
+        )
+
+        BasicTextField(
+            value = userValue,
+            onValueChange = { userValue = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(colors.primary)
+                .padding(8.dp),
+            textStyle = TextStyle(
+                fontFamily = suite,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 18.sp,
+                color = colors.onPrimary,
+                textAlign = TextAlign.Start
+            ),
+        )
+    }
+}
+
+@Composable
+fun TextFieldFormat() {}
+
+
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter", "CoroutineCreationDuringComposition")
-@OptIn(ExperimentalMaterialApi::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun ProfileEditScreen(sheetState: ModalBottomSheetState) {
     val coroutineScope = rememberCoroutineScope()
     val roundedCornerRadius = 12.dp
-    var hasRightButton = true //오른쪽 버튼이 있는지 여부
 
     ModalBottomSheetLayout(
         sheetState = sheetState,
@@ -133,139 +245,6 @@ fun ProfileEditScreen(sheetState: ModalBottomSheetState) {
         sheetContent = {
             coroutineScope.launch {
                 sheetState.animateTo(ModalBottomSheetValue.Expanded)
-            }
-            Column(modifier = Modifier.fillMaxHeight()) {
-                MaterialTheme { //맨 위의 앱 바
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 8.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Button( // 뒤로 가기 버튼
-                            onClick = {
-                                coroutineScope.launch {
-                                    sheetState.hide()
-                                    settingScreen = "default"
-                                }
-                            },
-                            modifier = Modifier.align(Alignment.CenterStart),
-                            elevation = ButtonDefaults.elevation(
-                                defaultElevation = 0.dp,
-                                pressedElevation = 0.dp
-                        ),
-                        ) {
-                            Icon(
-                                Icons.Filled.ArrowBack,
-                                contentDescription = "Back"
-                            )
-                        }
-
-                        Text(
-                            text = "프로필 편집",
-                            fontFamily = suite,
-                            fontWeight = FontWeight.ExtraBold,
-                            fontSize = 20.sp,
-                            textAlign = TextAlign.Center,
-                            color = Color.Black,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-
-                        if (hasRightButton) {
-                            Button(
-                                onClick = { /* 오른쪽 버튼 클릭 시 실행될 코드 */ },
-                                modifier = Modifier.align(Alignment.CenterEnd)
-                            ) {
-                                Text("저장")
-                            }
-                        }
-                    }
-                }
-
-                Column( //페이지 내용
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 24.dp)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .padding(top = 16.dp)
-                            .fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.logo),
-                            contentDescription = "Profile Picture",
-                            modifier = Modifier
-                                .size(100.dp)
-                                .clip(CircleShape)
-                                .border(1.dp, Color.LightGray, CircleShape)
-                        )
-                    }
-                    Button(
-                        onClick = { /* 사진 변경 기능 실행 */ },
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally)
-                            .padding(vertical = 4.dp),
-                        elevation = ButtonDefaults.elevation(
-                            defaultElevation = 0.dp,
-                            pressedElevation = 0.dp
-                        ),
-                        colors = ButtonDefaults.buttonColors(
-                            //버튼을 눌렀을 때 나오는 색 효과 제거
-                        )
-                    ) {
-                        Text(
-                            text = "프로필 사진 변경",
-                            fontFamily = suite,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 15.sp,
-                            color = Color.Blue
-                        )
-                    }
-
-                    var name by remember { mutableStateOf("") }
-
-                    Text(
-                        text = "이름",
-                        fontFamily = suite,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 15.sp,
-                        color = Color.Gray
-                    )
-
-                    val keyboardController = LocalSoftwareKeyboardController.current
-
-                    TextField(
-                        value = name,
-                        onValueChange = { name = it },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(48.dp)
-                            .background(White)
-                            .padding(0.dp)
-                            .onKeyEvent { keyEvent ->
-                                if (keyEvent.key == Key.Enter) {
-                                    keyboardController?.hide() // 키보드 숨기기
-                                    true
-                                } else { false }
-                            },
-                        textStyle = TextStyle(
-                            fontFamily = suite,
-                            fontWeight = FontWeight.Normal,
-                            fontSize = 15.sp
-                        ),
-                        colors = TextFieldDefaults.textFieldColors(
-                            textColor = Black,
-                            disabledTextColor = Black,
-                            backgroundColor = Transparent,
-                            focusedIndicatorColor = DarkGray,
-                            unfocusedIndicatorColor = DarkGray,
-                            disabledIndicatorColor = DarkGray
-                        ),
-                        singleLine = true,
-                    )
-                }
             }
         }
     ) {}
@@ -290,7 +269,7 @@ fun LogoutPopupScreen() { //로그아웃 팝업창
                     fontFamily = suite,
                     fontWeight = FontWeight.ExtraBold,
                     fontSize = 17.sp,
-                    color = Color.Black
+                    color = colors.onPrimary
                 )
             },
             text = {
@@ -323,7 +302,7 @@ fun LogoutPopupScreen() { //로그아웃 팝업창
                             fontFamily = suite,
                             fontWeight = FontWeight.SemiBold,
                             fontSize = 13.sp,
-                            color = Color.Black
+                            color = colors.onPrimary
                         )
                     }
 
@@ -380,7 +359,7 @@ fun LogoutPopupScreen() { //로그아웃 팝업창
                             fontFamily = suite,
                             fontWeight = FontWeight.SemiBold,
                             fontSize = 13.sp,
-                            color = Color.Black
+                            color = colors.onPrimary
                         )
                     }
                 }
@@ -453,7 +432,7 @@ private fun AppInfoScreen(
                     }
                     for (name in names) {
                         Surface(
-                            modifier = Modifier.background(MaterialTheme.colors.primary)
+                            modifier = Modifier.background(colors.primary)
                         ) {
                             Column(
                                 modifier = Modifier.fillMaxWidth()
--- app/src/main/java/com/example/gd/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gd/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.LightGray
 
 private val DarkColorPalette = darkColors(
     onPrimary = Blue,
@@ -14,20 +15,14 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    onPrimary = Point,
-    primary = White,
-    primaryVariant = CustomGray,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onPrimary = Black, // 글자색
+    primary = White, // 배경색
+    primaryVariant = Point, // 포인트색
+    secondary = Gray, // 선택안된 BotomScreen icon & 검색창 돋보기와 X 아이콘
+    onSecondary = CustomGray, // // 검색창 색
+    secondaryVariant = LightGray // 검색창 글씨
 )
+
 
 @Composable
 fun GdTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {

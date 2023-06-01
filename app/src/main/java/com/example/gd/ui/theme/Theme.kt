1e9092e60fb5c4e2755bef363a9b28bd6603eb95
Dongmin <pdm001125@naver.com>
2023-06-01 09:56:09 +0900

    [FEAT] 다크모드 금지 설정
    다크모드 금지 설정

--- app/src/main/java/com/example/gd/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gd/ui/theme/Theme.kt
@@ -37,11 +37,15 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun GdTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
+    val colors = LightColorPalette
+        /*
+        if (darkTheme) {
         DarkColorPalette
     } else {
         LightColorPalette
     }
+
+         */
 
     MaterialTheme(
         colors = colors,
